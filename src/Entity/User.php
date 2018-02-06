5ef35a23702d84b6511b1944c9905515898b0ca3
Manuele Vaccari <manuele.vaccari@gmail.com>
2018-02-06 16:37:58 +0100

    Add return type
    * Minor change: Return type added to function

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -443,7 +443,7 @@ class User extends EncryptableFieldEntity implements UserInterface, \Serializabl
      *
      * @return void
      */
-    public function addFriend(User $user)
+    public function addFriend(User $user): void
     {
         if ($this->myFriends->contains($user)) {
             return;
@@ -457,7 +457,7 @@ class User extends EncryptableFieldEntity implements UserInterface, \Serializabl
      *
      * @return void
      */
-    public function removeFriend(User $user)
+    public function removeFriend(User $user): void
     {
         if (!$this->myFriends->contains($user)) {
             return;
